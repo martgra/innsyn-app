c1e6e04d3172d1d57c1935a16c4f455cb656f6b2
Martin Gran <martgra@gmail.com>
2020-09-11 23:13:18 +0200

    added search field and AJAX routine towards mock

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Search from './components/Search';
 
 function App() {
   return (
@@ -15,9 +16,11 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+         
         >
           Learn React
         </a>
+        <Search search_placeholder="Skriv inn søkeord" button="Trykk meg" url="https://892cc57b-1302-4256-a43e-caf341ab9547.mock.pstmn.io/api/"/>
       </header>
     </div>
   );
---
+++ src/components/Search.tsx
@@ -0,0 +1,56 @@
+// src/components/Hello.tsx
+
+import * as React from 'react';
+import { MouseEvent } from 'react';
+import './Search.css';
+
+export interface Props {
+  search_placeholder: string;
+  button: string;
+  url: string;
+}
+
+export interface State {
+  search_state?: string;
+}
+
+
+class Search extends React.Component<Props, State> {
+    constructor(props: Props) {
+      super(props);
+      this.state = {search_state: props.search_placeholder}; 
+    }
+    onSearch(event: MouseEvent) {
+      const url_append = this.props.url + this.state.search_state;
+
+      fetch(url_append, {method: 'GET'})
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result)
+          this.setState({
+            search_state: result.body
+          });
+          });
+        }
+      
+      
+
+    handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      this.setState({search_state: e.target.value});
+      //e.preventDefault();
+   }
+    render() {  
+  
+      return (
+        <div className="search">
+          <div className="searchField">
+            <input type="text" placeholder={this.props.search_placeholder} value={this.state.search_state} onChange={this.handleChange}/>
+            <button className="Button" onClick={this.onSearch.bind(this)}>{this.props.button}</button>
+          </div>
+        </div>
+      );
+    }
+  }
+
+  export default Search;
